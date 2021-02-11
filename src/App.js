68bacae26ab4661ca5b0662e8891875652811495
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2021-02-10 21:37:33 -0500

    Build hero component and add create home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact /> 
+          <Route path='/' exact component={Home}/> 
         </Switch>
       </Router>
     </>
---
+++ src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import Button from "./Button"
+import './HeroSection.css';
+
+export const HeroSection = () => {
+    return (
+        <div className='hero-container'>
+            <video src='/videos/skateclip.mp4' autoPlay loop muted />
+            <h1>Adventure away</h1>
+            <p>What are you waiting for</p>
+            <div className='hero-buttons'>
+                <Button 
+                    className='btns'
+                    buttonStyle='btn--outline'
+                    buttonSize='btn-large'
+                >
+                    Get Started
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection;
